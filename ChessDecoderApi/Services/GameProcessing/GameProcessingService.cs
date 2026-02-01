ec8bb50579ff92b7d5dc4d5b38393186f1b9ba8b
Paschalis Rompanos <pas.rompan@gmail.com>
2026-02-01 11:50:29 +0200

    fix test

--- ChessDecoderApi/Services/GameProcessing/GameProcessingService.cs
+++ ChessDecoderApi/Services/GameProcessing/GameProcessingService.cs
@@ -261,66 +261,108 @@ public class GameProcessingService : IGameProcessingService
         };
     }
 
-    public async Task<GameProcessingResponse> ProcessMockUploadAsync(IFormFile image, bool autoCrop = false)
+    public Task<GameProcessingResponse> ProcessMockUploadAsync(IFormFile image, bool autoCrop = false)
     {
-        _logger.LogInformation("Processing mock upload with autoCrop: {AutoCrop}", autoCrop);
+        _logger.LogInformation("Processing mock upload - returning hardcoded mock response");
 
-        var fileName = $"{Guid.NewGuid()}{Path.GetExtension(image.FileName)}";
-        var tempFilePath = Path.Combine(Path.GetTempPath(), fileName);
+        return Task.FromResult(GetMockGameProcessingResponse());
+    }
 
-        using (var stream = new FileStream(tempFilePath, FileMode.Create))
+    private static GameProcessingResponse GetMockGameProcessingResponse()
+    {
+        var mockPgn = "[Date \"????.??.??\"]\n[White \"?\"]\n[Black \"?\"]\n[Result \"*\"]\n\n1. e4 c5 \n 2. Nf3 Nc6 \n 3. d4 cxd4 \n 4. Nxd4 Nf6 \n 5. Nc3 e5 \n 6. Nf3 Be7 \n 7. Bc4 O-O \n 8. O-O a6 \n 9. Bd2 b5 \n 10. Bb3 Bb7 \n 11. Re1 d6 \n 12. Be3 Na5 \n 13. Bd5 Nxd5 \n 14. Nxd5 Nc4 \n 15. Nxe7+ Qxe7 \n 16. Bc1 Rac8 \n 17. b3 Nb6 \n 18. Be3 Nd7 \n 19. Qd3 f5 \n 20. Ng5 f4 \n 21. Nf3 fxe3 \n 22. Rxe3 Nc5 \n 23. Qe2 Rc6 \n 24. Rd1 Rfc8 \n 25. Qd2 Ne6 \n 26. c3 Qc7 \n 27. Rd3 Nf4 \n 28. Rxd6 Rxd6 \n 29. Qxd6 Qxd6 \n 30. Rxd6 Ne2+ \n 31. Kf1 Nxc3 \n 32. Rd7 Bxe4 \n 33. Nxe5 Re8 \n 34. Nf3 Bxf3 \n 35. gxf3 Nxa2 \n 36. Kg2 a5 \n 37. Ra7 b4 \n 38. Rxa5 Nc1 \n 39. Rb5 Nd3 \n 40. Rd5 Nf4+ \n 41. Kg3 Nxd5 \n 42. f4 Rf8 \n 43. f3 Rxf4 \n 44. h3 Rd4 \n 45. f4 Rd3+ \n 46. Kg4 Rxb3 \n 47. f5 Rc3 \n 48. f6 Nxf6+ \n 49. Kg5 b3 \n 50. h4 b2 \n 51. Kf4 b1=Q \n 52. Ke5 Qe4+ \n 53. Kd6 Rc6# \n *\n";
+
+        var mockMoves = new List<ChessMovePair>
         {
-            await image.CopyToAsync(stream);
-        }
-
-        string imagePathForProcessing = tempFilePath;
-        string? processedImageBase64 = null;
-
-        if (autoCrop)
-        {
-            using var originalImage = Image.Load<Rgba32>(tempFilePath);
-            var tableBoundaries = _legacyImageProcessingService.FindTableBoundaries(originalImage);
-            
-            var croppedImageBytes = await _imageManipulationService.CropImageAsync(
-                tempFilePath, 
-                tableBoundaries.X, 
-                tableBoundaries.Y, 
-                tableBoundaries.Width, 
-                tableBoundaries.Height);
-
-            var croppedFileName = $"{Guid.NewGuid()}_cropped{Path.GetExtension(image.FileName)}";
-            var croppedFilePath = Path.Combine(Path.GetTempPath(), croppedFileName);
-            await File.WriteAllBytesAsync(croppedFilePath, croppedImageBytes);
-            
-            imagePathForProcessing = croppedFilePath;
-        }
-
-        var result = await _imageExtractionService.ProcessImageAsync(imagePathForProcessing);
-
-        // Generate image
-        var imageBytes = await File.ReadAllBytesAsync(imagePathForProcessing);
-        processedImageBase64 = Convert.ToBase64String(imageBytes);
-
-        // Clean up
-        try
-        {
-            if (File.Exists(tempFilePath)) File.Delete(tempFilePath);
-            if (autoCrop && imagePathForProcessing != tempFilePath && File.Exists(imagePathForProcessing)) 
-                File.Delete(imagePathForProcessing);
-        }
-        catch (Exception ex)
-        {
-            _logger.LogWarning(ex, "Failed to clean up temp files");
-        }
+            CreateMovePair(1, "e4", "c5"),
+            CreateMovePair(2, "Nf3", "Nc6"),
+            CreateMovePair(3, "d4", "cxd4"),
+            CreateMovePair(4, "Nxd4", "Nf6"),
+            CreateMovePair(5, "Nc3", "e5"),
+            CreateMovePair(6, "Nf3", "Be7"),
+            CreateMovePair(7, "Bc4", "O-O"),
+            CreateMovePair(8, "O-O", "a6"),
+            CreateMovePair(9, "Bd2", "b5"),
+            CreateMovePair(10, "Bb3", "Bb7"),
+            CreateMovePair(11, "Re1", "d6"),
+            CreateMovePair(12, "Be3", "Na5"),
+            CreateMovePair(13, "Bd5", "Nxd5"),
+            CreateMovePair(14, "Nxd5", "Nc4"),
+            CreateMovePair(15, "Nxe7+", "Qxe7"),
+            CreateMovePair(16, "Bc1", "Rac8"),
+            CreateMovePair(17, "b3", "Nb6"),
+            CreateMovePair(18, "Be3", "Nd7"),
+            CreateMovePair(19, "Qd3", "f5"),
+            CreateMovePair(20, "Ng5", "f4"),
+            CreateMovePair(21, "Nf3", "fxe3"),
+            CreateMovePair(22, "Rxe3", "Nc5"),
+            CreateMovePair(23, "Qe2", "Rc6"),
+            CreateMovePair(24, "Rd1", "Rfc8"),
+            CreateMovePair(25, "Qd2", "Ne6"),
+            CreateMovePair(26, "c3", "Qc7"),
+            CreateMovePair(27, "Rd3", "Nf4"),
+            CreateMovePair(28, "Rxd6", "Rxd6"),
+            CreateMovePair(29, "Qxd6", "Qxd6"),
+            CreateMovePair(30, "Rxd6", "Ne2+"),
+            CreateMovePair(31, "Kf1", "Nxc3"),
+            CreateMovePair(32, "Rd7", "Bxe4"),
+            CreateMovePair(33, "Nxe5", "Re8"),
+            CreateMovePair(34, "Nf3", "Bxf3"),
+            CreateMovePair(35, "gxf3", "Nxa2"),
+            CreateMovePair(36, "Kg2", "a5"),
+            CreateMovePair(37, "Ra7", "b4"),
+            CreateMovePair(38, "Rxa5", "Nc1"),
+            CreateMovePair(39, "Rb5", "Nd3"),
+            CreateMovePair(40, "Rd5", "Nf4+"),
+            CreateMovePair(41, "Kg3", "Nxd5"),
+            CreateMovePair(42, "f4", "Rf8"),
+            CreateMovePair(43, "f3", "Rxf4"),
+            CreateMovePair(44, "h3", "Rd4"),
+            CreateMovePair(45, "f4", "Rd3+"),
+            CreateMovePair(46, "Kg4", "Rxb3"),
+            CreateMovePair(47, "f5", "Rc3"),
+            CreateMovePair(48, "f6", "Nxf6+"),
+            CreateMovePair(49, "Kg5", "b3"),
+            CreateMovePair(50, "h4", "b2"),
+            CreateMovePair(51, "Kf4", "b1=Q"),
+            CreateMovePair(52, "Ke5", "Qe4+"),
+            CreateMovePair(53, "Kd6", "Rc6#")
+        };
 
         return new GameProcessingResponse
         {
-            GameId = Guid.NewGuid(),
-            PgnContent = result.PgnContent,
-            Validation = result.Validation,
+            GameId = Guid.Parse("9e0c1155-6c36-43e6-9160-7e00546f9c87"),
+            PgnContent = mockPgn,
+            Validation = new ChessGameValidation
+            {
+                GameId = "a013494e-e64a-47af-9d96-7b727f4f0b69",
+                Moves = mockMoves
+            },
             ProcessingTimeMs = 0,
             CreditsRemaining = 100,
-            ProcessedImageUrl = $"data:image/png;base64,{processedImageBase64}"
+            ProcessedImageUrl = null
+        };
+    }
+
+    private static ChessMovePair CreateMovePair(int moveNumber, string whiteNotation, string blackNotation)
+    {
+        return new ChessMovePair
+        {
+            MoveNumber = moveNumber,
+            WhiteMove = new Models.ValidatedMove
+            {
+                Notation = whiteNotation,
+                NormalizedNotation = whiteNotation,
+                ValidationStatus = "valid",
+                ValidationText = ""
+            },
+            BlackMove = new Models.ValidatedMove
+            {
+                Notation = blackNotation,
+                NormalizedNotation = blackNotation,
+                ValidationStatus = "valid",
+                ValidationText = ""
+            }
         };
     }
 
